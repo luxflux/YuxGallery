fdf7fcfa05277af6b67bfd05efb662dff345e6bd
Raffael Schmid <raffael@yux.ch>
2011-02-12 13:57:53 +0100

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YuxGallery::Application.config.secret_token = '00a5041beb637b915fc9f1de1621c9c38adb4833a17f53e83a4b49323dfd67f73fce6bb7843e18044a2a6fda3280ad4de73dceac028fd662434a979af44c4efc'
