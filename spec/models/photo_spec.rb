37e847d114e58c2276bd9dd4078b4fcbf1987a08
Raffael Schmid <raffael@yux.ch>
2011-07-21 23:16:45 +0200

    more photo tests

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -6,9 +6,34 @@ describe Photo do
   end
 
   it "can be saved successfully" do
-    PhotoUploader.any_instance.stubs(:get_exif_data).returns({
-      :date_time_original => Time.now
-    })
     FactoryGirl.create(:photo).should be_persisted
   end
+  
+  it "requires a name" do
+    Photo.create.should have(1).errors_on(:name)
+  end
+
+  it "responds on title and returns the name" do
+    p = FactoryGirl.create(:photo)
+    p.title.should equal(p.name)
+  end
+
+  it "returns the user which owns this album when calling user" do
+    u = FactoryGirl.create(:user)
+    a = FactoryGirl.create(:album, :user_id => u.id)
+    FactoryGirl.create(:photo, :album_id => a.id).user.should be_an_instance_of(User)
+  end
+
+  it "can read the exif data from its photo" do
+    FactoryGirl.create(:photo).exif_data[:software].should == "MySoftware 1.2"
+  end
+
+  it "can set its attributes from the exif data" do
+    p = FactoryGirl.create(:photo, :name => "Photo1", :shot_at => 1.year.ago, :description => "Testing")
+    p.set_from_exif!
+    p.name.should == "test.png"
+    p.shot_at.should == Time.now
+    p.description.should == "Created with Testcam, edited with MySoftware 1.2"
+  end
+
 end
