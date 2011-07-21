62586b33786437c2c900b5653ee9c5c9c6234325
Raffael Schmid <raffael@yux.ch>
2011-07-21 21:23:40 +0200

    add rspec

---
+++ spec/models/album_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Album do
+  it "can be instantiated" do
+    Album.new.should be_an_instance_of(Album)
+  end
+
+  it "can be saved successfully" do
+    Album.create.should be_persisted
+  end
+end
