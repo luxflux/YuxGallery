54418175d49050787aac7c8d9ba36bad0f6184c8
Raffael Schmid <raffael@yux.ch>
2011-08-13 14:22:12 +0200

    make code more readable and better for rcov

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,24 +41,35 @@ module ApplicationHelper
   end
 
   def form_auth_token
-    (protect_against_forgery?) ? form_authenticity_token : ''
+    if protect_against_forgery?
+      form_authenticity_token
+    else
+      ''
+    end
   end
 
   def yux_gallery_path_get_title(model)
-    if model.title
+    if model.respond_to?(:title)
       model.title
     else
-      t("#{model.class.name.tableize}.new.title")
+      I18n.t("#{model.class.name.tableize}.title", :id => model.id)
     end
   end
 
   def yux_gallery_path
     content_tag :ul, :class => :gallery_path do
       p =  content_tag(:li, link_to(t(".path.start"), root_url))
-      p += content_tag(:li, link_to(yux_gallery_path_get_title(@user),  [@user, :albums]))         if @user && !@user.new_record?
-      p += content_tag(:li, link_to(yux_gallery_path_get_title(@album), [@user, @album, :photos])) if @album && !@album.new_record?
-      p += content_tag(:li, link_to(yux_gallery_path_get_title(@photo), [@user, @album, @photo]))  if @photo
-      p += content_tag(:li, link_to(yux_gallery_path_get_title(@scan),  [@user, @album, @scan]))   if @scan
+      if @user && !@user.new_record?
+        p += content_tag(:li, link_to(yux_gallery_path_get_title(@user),  [@user, :albums]))
+      end
+      if @album && !@album.new_record?
+        p += content_tag(:li, link_to(yux_gallery_path_get_title(@album), [@user, @album, :photos]))
+      end
+      if @photo
+        p += content_tag(:li, link_to(yux_gallery_path_get_title(@photo), [@user, @album, @photo]))
+      elsif @scan
+        p += content_tag(:li, link_to(yux_gallery_path_get_title(@scan),  [@user, @album, @scan]))
+      end
       p
     end
   end
@@ -82,7 +93,6 @@ module ApplicationHelper
     content = ""
     content_tag :ul, :class => :photo_collection do
       collection.each do |item|
-        url = prefix ? [ prefix, item ] : item
         url = case
           when prefix && postfix
             [ prefix, item, postfix ]
@@ -93,8 +103,19 @@ module ApplicationHelper
           else
             item
         end
-        item_photo = item.respond_to?(:random_photo) ? item.random_photo : item
-        item_photo_url = item_photo.respond_to?(:photo) ? item_photo.photo.thumb.url : yux_default_photo
+
+        item_photo = if item.respond_to?(:random_photo)
+                       item.random_photo
+                     else
+                       item
+                     end
+        
+        item_photo_url = if item_photo.respond_to?(:photo)
+                           item_photo.photo.thumb.url 
+                         else
+                           yux_default_photo
+                         end
+        
         content += content_tag :li do
           yux_link_to_with_photo(item_photo_url, url, :title => item.title)
         end
@@ -132,9 +153,11 @@ module ApplicationHelper
   end
 
   def update_lightbox(title = nil)
-    yield
-    content = "Lightbox.current.dialog.resize();"
-    content += "Lightbox.current.dialog.setTitle('#{title}')" if title
+    content = "#{yield}"
+    content += "Lightbox.current.dialog.resize();"
+    if title
+      content += "Lightbox.current.dialog.setTitle('#{title}')"
+    end
     content
   end
 
@@ -143,7 +166,9 @@ module ApplicationHelper
   end
 
   def label_with_tooltip(f, name, tooltip = false)
-    tooltip = t(".tooltip.#{name}") unless tooltip
+    unless tooltip
+      tooltip = t("tooltip.#{f.object_name}.#{name}")
+    end
     f.label(name, :title => tooltip, :"data-tooltip" => true) + icon_tag(:help, :title => tooltip, :"data-tooltip" => true)
   end
 
