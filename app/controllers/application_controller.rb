058601b3ce984a6b7721d7d17ee25d3897e74ac1
Raffael Schmid <raffael@yux.ch>
2011-08-13 14:21:43 +0200

    make code more readable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,10 @@ class ApplicationController < ActionController::Base
   alias_method :t_original, :t
   def t(*args)
     if args.first[0..1] == '..'
-      args[0] = params[:controller].gsub('/','.')+args.first[1..-1]
+      args[0] = params[:controller].gsub('/','.') + args.first[1..-1]
     elsif args.first.first == '.'
-      args[0] = params[:controller].gsub('/','.')+'.'+params[:action]+args.first    end
+      args[0] = params[:controller].gsub('/','.') + '.' + params[:action] + args.first
+    end
     t_original(*args)
   end
 
