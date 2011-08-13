28ca35188578c1eaa9c07bab1618ee8d6b805b2c
Raffael Schmid <raffael@yux.ch>
2011-08-13 15:23:57 +0200

    change spec name

--- spec/helpers/photos_helper_spec.rb
+++ spec/helpers/photos_helper_spec.rb
@@ -1,14 +1,7 @@
 require "spec_helper"
 
-describe ApplicationHelper do
+describe PhotosHelper do
  
-  before do
-    @user  = FactoryGirl.create(:user)
-    @user2 = FactoryGirl.create(:user)
-    @album = FactoryGirl.create(:album, :user_id => @user.id)
-    @photo = FactoryGirl.create(:photo, :album_id => @album.id)
-  end
-
   describe "update_elements_js" do
     it "renders javascript to update several elements with content taken from another element" do
       update_elements_js("testfield", [ "ele1", "ele2" ]).should
