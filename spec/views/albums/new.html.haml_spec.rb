9a59715c6b25720ab61bb9d19e7489fb186df2b9
Raffael Schmid <raffael@yux.ch>
2011-08-20 15:03:59 +0200

    haml for albums#new

---
+++ spec/views/albums/new.html.haml_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe "albums/new.html.haml" do
+  before do
+    stub_template "_form" => "rendered form"
+    render
+  end
+
+  it "renders the form partial" do
+    rendered.should eq("rendered form\n")
+  end
+end
