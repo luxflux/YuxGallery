ae43aa12f1a7af23e3d5516309c854cc3239268d
Raffael Schmid <raffael@yux.ch>
2011-08-20 12:53:59 +0200

    move photos#index to haml

---
+++ spec/views/photos/index.html.haml_spec.rb
@@ -0,0 +1,56 @@
+require "spec_helper"
+
+describe "photos/index.html.haml" do
+  before do
+    @user = stub_model(User)
+    @album = stub_model(Album, :user => @user)
+    @photos = []
+    assign(:user, @user)
+    assign(:album, @album)
+    view.expects(:content_for).with(:title, "#{@user.title} / #{@album.title}").returns("#{@user.title} / #{@album.title}")
+  end
+
+  context "without a photo" do
+    before do
+      assign(:scans, @photos)
+      view.expects(:t).with(".this_album_has_photos", :count => 0).returns("This album doesn't have a photo")
+      view.expects(:content_for).with(:actions).returns("New Photo<br />New Scan")
+      view.expects(:current_user).returns(@user)
+      render
+    end
+   
+    subject do
+      rendered
+    end
+    it { should_not have_selector("ul.rui-tabs-carousel") }
+    it { should_not have_selector("ul.rui-tabs-carousel ul") }
+    it { should_not have_selector("ul.rui-tabs-carousel ul li a img") }
+
+  end
+  
+  context "with multiple photos" do
+    before do
+      4.times do
+        @photos << stub_model(Photo, :album => @album)
+      end
+      assign(:scans, @photos)
+    end
+
+    context "and they belong to the logged in user" do
+      before do
+        view.expects(:t).with(".this_album_has_photos", :count => 4).returns("This album has 4 photos")
+        view.expects(:content_for).with(:actions).returns("New Photo<br />New Scan")
+        view.expects(:current_user).returns(@user)
+        render
+      end
+ 
+      subject do
+        rendered
+      end
+
+      it { should have_selector("ul.rui-tabs-carousel") }
+      it { should have_selector("ul.rui-tabs-carousel ul") }
+      it { should have_selector("ul.rui-tabs-carousel ul li a img", :count => 4) }
+    end
+  end
+end
