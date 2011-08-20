d7413a08a1c78fbc79fa8dace6ec5dcf61341ff2
Raffael Schmid <raffael@yux.ch>
2011-08-20 13:34:26 +0200

    move layout _update_lightbox_with_errors_for to haml

---
+++ spec/views/layouts/_update_lightbox_with_errors_for.js.haml_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe "layouts/_update_lightbox_with_errors_for.js.haml" do
+  before do
+    @user = stub_model(User)
+    assign(:user, @user)
+    view.expects(:model).returns(@user)
+    render
+  end
+  
+  subject do
+    rendered
+  end
+
+  it { should match(/\$\('error_explanation'\).replace/) }
+  it { should match(/\$\('error_explanation'\).show\(\)/) }
+  it { should match(/Lightbox.current.dialog.resize\(\)/) }
+end
